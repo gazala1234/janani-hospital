4c5c5ca3302d21becc902cfac8ab0b014f44d5a8
LubnaAjanal <lubnaajanal@gmail.com>
2024-06-10 15:58:27 +0530

    Lubna: added all the bootstrap and template links in one file

---
+++ resources/views/links.blade.php
@@ -0,0 +1,34 @@
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="{{ asset('css/mainstyle/style.css') }}" rel="stylesheet">
+
+
+    {{-- js links --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+     <!-- Vendor JS Files -->
+     <script src="{{ asset('js/apexcharts.min.js') }}"></script>
+     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+     <script src="{{ asset('js/chart.umd.js') }}"></script>
+     <script src="{{ asset('js/echarts.min.js') }}"></script>
+     <script src="{{ asset('js/quill.js') }}"></script>
+     <script src="{{ asset('js/simple-datatables.js') }}"></script>
+     <script src="{{ asset('js/tinymce.min.js') }}"></script>
+     <script src="{{ asset('js/validate.js') }}"></script>
+ 
+     <!-- Template Main JS File -->
+     <script src="{{ asset('js/mainjs/main.js') }}"></script>
