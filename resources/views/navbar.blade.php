106e8dcab28a8a3193568cc4fcba8e0cd6871b23
gazala1234 <ustadgazal@gmail.com>
2024-06-10 11:40:59 +0530

    gazala: fetch name and id dynamically

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -47,12 +47,12 @@
         <!-- End Logo -->
 
         <!-- start Search Bar -->
-        <div class="search-bar">
+        {{-- <div class="search-bar">
             <form class="search-form d-flex align-items-center" method="POST" action="#">
                 <input type="text" name="query" placeholder="Search" title="Enter search keyword">
                 <button type="submit" title="Search"><i class="bi bi-search"></i></button>
             </form>
-        </div>
+        </div> --}}
         <!-- End Search Bar -->
 
         <!-- start Search Icon-->
@@ -212,13 +212,13 @@
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                         data-bs-toggle="dropdown">
                         <img src="{{ asset('images/profile.png') }}" alt="Profile" class="rounded-circle">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">Lubna Ajnal</span>
+                        <span class="d-none d-md-block dropdown-toggle ps-2">{{ $user->name }}</span>
                     </a><!-- End Profile Iamge Icon -->
 
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                         <li class="dropdown-header">
-                            <h6>Kevin Anderson</h6>
-                            <span>Emp_Id:</span>
+                            <h6>{{ $user->name }}</h6>
+                            <span>Employee_Id: {{ $user->eid }}</span>
                         </li>
                         <li>
                             <hr class="dropdown-divider">
