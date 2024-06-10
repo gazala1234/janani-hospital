06b72c59bbb03ab6f0bfd0f82c7142fcb1f77b50
LubnaAjanal <lubnaajanal@gmail.com>
2024-06-10 16:15:57 +0530

    Lubna: deleted boot file and created links file and added all bootstrap and template links in one file

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,10 +1,8 @@
-    <!-- Google Fonts -->
+<!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
+<!-- Vendor CSS Files -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('css/boxicons.min.css') }}" rel="stylesheet">
@@ -13,22 +11,31 @@
     <link href="{{ asset('css/remixicon.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
+<!-- Template Main CSS File -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="{{ asset('css/mainstyle/style.css') }}" rel="stylesheet">
 
+    
+<!-- Vendor JS Files -->
+    <script src="{{ asset('js/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/chart.umd.js') }}"></script>
+    <script src="{{ asset('js/echarts.min.js') }}"></script>
+    <script src="{{ asset('js/quill.js') }}"></script>
+    <script src="{{ asset('js/simple-datatables.js') }}"></script>
+    <script src="{{ asset('js/tinymce.min.js') }}"></script>
+    <script src="{{ asset('js/validate.js') }}"></script>
 
-    {{-- js links --}}
+    <!-- Template Main JS File -->
+    <script src="{{ asset('js/mainjs/main.js') }}"></script>
+
+    {{-- Our bootstrap links --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-     <!-- Vendor JS Files -->
-     <script src="{{ asset('js/apexcharts.min.js') }}"></script>
-     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-     <script src="{{ asset('js/chart.umd.js') }}"></script>
-     <script src="{{ asset('js/echarts.min.js') }}"></script>
-     <script src="{{ asset('js/quill.js') }}"></script>
-     <script src="{{ asset('js/simple-datatables.js') }}"></script>
-     <script src="{{ asset('js/tinymce.min.js') }}"></script>
-     <script src="{{ asset('js/validate.js') }}"></script>
- 
-     <!-- Template Main JS File -->
-     <script src="{{ asset('js/mainjs/main.js') }}"></script>
