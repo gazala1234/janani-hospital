52470e64ea8c7be9b1ca6c381aecc978cad3f4ab
gazala1234 <ustadgazal@gmail.com>
2024-10-24 20:00:11 +0530

    corrected styling

--- public/js/customjs/chatting.js
+++ public/js/customjs/chatting.js
@@ -1,52 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Enable rich text formatting for the contenteditable div
-    $('#boldBtn').click(function() {
-        document.execCommand('bold');
+    $("#boldBtn").click(function () {
+        document.execCommand("bold");
     });
 
-    $('#italicBtn').click(function() {
-        document.execCommand('italic');
+    $("#italicBtn").click(function () {
+        document.execCommand("italic");
     });
 
-    $('#underlineBtn').click(function() {
-        document.execCommand('underline');
+    $("#underlineBtn").click(function () {
+        document.execCommand("underline");
     });
 
-    $('#listBtn').click(function() {
-        document.execCommand('insertUnorderedList');
+    $("#listBtn").click(function () {
+        document.execCommand("insertUnorderedList");
     });
 
     // Trigger photo input when photo icon is clicked
-    $('#photoIcon').click(function() {
-        $('#photoInput').click();
+    $("#photoIcon").click(function () {
+        $("#photoInput").click();
     });
 
     // Trigger video input when video icon is clicked
-    $('#videoIcon').click(function() {
-        $('#videoInput').click();
+    $("#videoIcon").click(function () {
+        $("#videoInput").click();
     });
 
     // Handle file selection for photo
-    $('#photoInput').change(function() {
+    $("#photoInput").change(function () {
         alert("Photo selected: " + this.files[0].name);
     });
 
     // Handle file selection for video
-    $('#videoInput').change(function() {
+    $("#videoInput").change(function () {
         alert("Video selected: " + this.files[0].name);
     });
 
-    $('.comment-icon').click(function() {
-        $(this).closest('.post-card').find('.chatbox').toggle(); // Toggle the chatbox
-    });
-
-    // Handle comment submission
-    $('.submit-comment').click(function() {
-        var comment = $(this).siblings('.comment-input').val(); // Get the input value
-        if (comment.trim() !== '') {
-            var chatbox = $(this).siblings('.chat');
-            chatbox.append('<div class="chat-message">' + comment + '</div>'); // Append the comment
-            $(this).siblings('.comment-input').val(''); // Clear the input
-        }
+    // comment box js
+    $("#commentIcon").click(function () {
+        // Toggle the comment box below the icons
+        $(this).closest(".post-actions").next(".comment-box").toggle();
     });
 });
